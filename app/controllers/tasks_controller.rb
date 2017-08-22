b6f41c40252cdc1ecbd5b72f3b4def863a7f8aa7
masayakamiunten <s15b1014@gmail.com>
2017-08-22 06:56:07 +0000

    tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,8 @@
 class TasksController < ApplicationController
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
   def create
     @task = current_user.tasks.build(task_params)
-
     if @task.save
       flash[:success] = 'Task が正常に作成されました'
       redirect_to @task
@@ -55,5 +56,12 @@ class TasksController < ApplicationController
   
   def task_params
     params.require(:task).permit(:content)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
